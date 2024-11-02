929c5b3f9c1fb983eb35c8c617348c9c389eab73
iMGamal <mohamed.gamal126027@gmail.com>
2024-11-03 00:51:21 +0200

    Demo

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,10 +3,46 @@
 
 
 def validUTF8(data):
-    """Check whether the passed data fits UTF-8."""
-    if len(data) == 0:
+    """Check 2-byte and 1-byte representation of UTF-8."""
+    new = []
+
+    if data == []:
         return False
-    elif len(data) in range(1, 5):
+
+    for i in range(len(data)):
+        if data[i] == 0:
+            return False
+
+        temp2 = data[i]
+        b = ''
+
+        while temp2 > 0:
+            temp1 = temp2 % 2
+            n = str(temp1)
+            b += n
+            temp2 = temp2 // 2
+
+        s = b[::-1]
+        if len(s) < 8:
+            s = s.zfill(8)
+        new.append(s)
+
+    if int(new[0][:5]) == 11110:
+        if int(new[1][:2]) == 10 and int(new[2][:2]) == 10 and int(new[3][:2]) == 10:
+            return True
+        else:
+            return False
+    elif int(new[0][:4]) == 1110:
+        if int(new[1][:2]) == 10 and int(new[2][:2]) == 10:
+            return True
+        else:
+            return False
+    elif int(new[0][:3]) == 110:
+        if int(new[1][:2]) == 10:
+            return True
+        else:
+            return False
+    elif int(new[0][:1]) == 0:
         return True
     else:
         return False
