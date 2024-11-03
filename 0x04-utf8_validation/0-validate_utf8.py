086db15646b89d3fb951584b13519c336e193ca9
iMGamal <mohamed.gamal126027@gmail.com>
2024-11-03 14:45:16 +0000

    Mixed version

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,31 +1,30 @@
-#!/usr/bin/python3
-"""There are no modules to be imported."""
-
-
 def validUTF8(data):
-    """Check for utf-8 encoding validity."""
-    num_bytes = 0
+    """Check if the data is a valid UTF-8 encoding."""
+    num_bytes = 0  # Tracks the expected number of continuation bytes
 
     for num in data:
+        # Check if `num` is within the valid byte range
         if num < 0 or num > 255:
             return False
-
-        binary = bin(num)[2:].zfill(8)
-
-        if num_bytes == 0:
-            if binary.startswith('0'):
-                continue
-            elif binary.startswith('110'):
-                num_bytes = 1
-            elif binary.startswith('1110'):
-                num_bytes = 2
-            elif binary.startswith('11110'):
-                num_bytes = 3
-            else:
-                return False
-        else:
-            if not binary.startswith('10'):
+        
+        # If we are expecting continuation bytes
+        if num_bytes > 0:
+            # Check if the byte starts with '10' (continuation byte)
+            if (num >> 6) != 0b10:
                 return False
             num_bytes -= 1
+        else:
+            # Determine the number of bytes in the UTF-8 character
+            if (num >> 7) == 0b0:
+                num_bytes = 0  # 1-byte character
+            elif (num >> 5) == 0b110:
+                num_bytes = 1  # 2-byte character
+            elif (num >> 4) == 0b1110:
+                num_bytes = 2  # 3-byte character
+            elif (num >> 3) == 0b11110:
+                num_bytes = 3  # 4-byte character
+            else:
+                return False  # Invalid UTF-8 character start
 
+    # All characters should be complete by the end
     return num_bytes == 0
---
+++ 0x04-utf8_validation/2-demo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""There are no modules to be imported."""
+
+
+def validUTF8(data):
+    """Check for utf-8 encoding validity."""
+    num_bytes = 0
+
+    for num in data:
+        if num < 0 or num > 255:
+            return False
+
+        binary = bin(num)[2:].zfill(8)
+
+        if num_bytes == 0:
+            if binary.startswith('0'):
+                continue
+            elif binary.startswith('110'):
+                num_bytes = 1
+            elif binary.startswith('1110'):
+                num_bytes = 2
+            elif binary.startswith('11110'):
+                num_bytes = 3
+            else:
+                return False
+        else:
+            if not binary.startswith('10'):
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
